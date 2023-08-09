f93895e2b107a243ae2144b4a585e8bafd6d8df5
Maksim <mirror0791@mail.ru>
2023-08-09 22:46:51 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SEMINARDZ2
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            int numberRnd = new Random().Next(100, 1000);
+            System.Console.WriteLine("Рандомное 3-х значное число: " + numberRnd);
+            int number = (numberRnd % 100) / 10; 
+            System.Console.WriteLine("Вторая цифра этого числа: " + number);
+        }
+    }    
+}
