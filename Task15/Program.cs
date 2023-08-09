9f9e202ff854d3f19f85313652bfb991f22d3fcf
Maksim <mirror0791@mail.ru>
2023-08-09 21:30:00 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SEMINARDZ2
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.Write("Введите число от 1 до 7 обозначающий день недели: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number >= 1 & number <= 7)
+            {
+                if (number == 6 || number == 7)
+                {
+                    Console.WriteLine ("Этот день является выходным");
+                }
+                else
+                {
+                    System.Console.WriteLine("Этот день не выходной");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Введено не корректное число!");
+            }
+        }
+    }
+}
