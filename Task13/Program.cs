f389d14b8b3b4515ec816902233fbb07254caa35
Maksim <mirror0791@mail.ru>
2023-08-10 20:55:04 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SEMINARDZ2
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.Write("Введите положительное целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number >= 100);
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                if (number > 99 & number < 1000)
+                {
+                    number = number % 10;
+                    System.Console.WriteLine("Третья цифра в данном числе: " + number);
+                }
+                else
+                System.Console.WriteLine("В данном числе нет третьей цифры");
+            }
+        }
+    }
+}
